920c13cd56e8ebb3ec8b4aa5b4815efb7a3b327e
Gustavo <theworldismine249@yahoo.com>
2016-05-03 22:11:03 +0300

    quicksort updated to a better version

--- Day 1/quickSortForStrings/src/Main.java
+++ Day 1/quickSortForStrings/src/Main.java
@@ -1,48 +1,54 @@
+import java.util.ArrayList;
+
 public class Main {
+    private static String[] data = new String[]{"a", "b", "c", "f", "b", "e", "b"};
 
     public static void main(String[] args) {
-        String[] list = {"a", "f", "c", "b"};
-        quickSort(list, 0, list.length - 1);
-        for (String s : list) {
-            System.out.println(s);
+        String[] sortedData = quickSort(data);
+
+        for (String str : sortedData) {
+            System.out.println(str);
         }
     }
 
-    private static void quickSort(String[] list, int start, int end) {
-        if (start < end) {
-            int pIndex = partition(list, start, end);
-            quickSort(list, start, pIndex - 1);
-            quickSort(list, pIndex + 1, end);
+    private static String[] quickSort(String[] data) {
+        if (data.length <= 1) {
+            return data;
         }
-    }
 
-    private static int partition(String[] list, int start, int end) {
-        String pivot = list[end];
+        String[] sortedData = new String[data.length];
 
-        int leftCounter = start;
-        int rightCounter = end;
+        String pivot = data[data.length - 1];
 
-        while (leftCounter < rightCounter) {
-            while (list[leftCounter].compareTo(pivot) <= 0 && leftCounter < end && rightCounter > leftCounter) {
-                leftCounter++;
-            }
-            while (list[rightCounter].compareTo(pivot) >= 0 && rightCounter > start && rightCounter >= leftCounter) {
-                rightCounter--;
-            }
-            if (leftCounter < rightCounter) {
-                swap(list, leftCounter, rightCounter);
-                leftCounter++;
-                rightCounter--;
+        ArrayList<String> lessThan = new ArrayList<>();
+        ArrayList<String> equalTo= new ArrayList<>();
+        ArrayList<String> greaterThan = new ArrayList<>();
+
+        for (String str : data) {
+            int compareValue = str.compareTo(pivot);
+
+            if (compareValue < 0) {
+                lessThan.add(str);
+            } else if (compareValue == 0) {
+                equalTo.add(str);
+            } else {
+                greaterThan.add(str);
             }
         }
-        if(start < rightCounter) swap(list, start, rightCounter);
-        if(end > leftCounter) swap(list, leftCounter, end);
-        return end;
-    }
 
-    private static void swap(String[] list, int start, int end) {
-        String aux = list[start];
-        list[start] = list[end];
-        list[end] = aux;
+        String[] lessThanSorted = quickSort(lessThan.toArray(new String[0]));
+        String[] greaterThanSorted = quickSort(greaterThan.toArray(new String[0]));
+
+        int length = 0;
+        for (String less: lessThanSorted) {
+            sortedData[length++] = less;
+        }
+        for (String equal : equalTo) {
+            sortedData[length++] = equal;
+        }
+        for (String greater : greaterThanSorted) {
+            sortedData[length++] = greater;
+        }
+        return sortedData;
     }
 }
